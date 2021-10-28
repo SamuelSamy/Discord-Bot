4463e210bfd4e25703d012e88d8968645f1e76ff
SamuelSamy <samuel.fatu@stud.ubbcluj.ro>
2021-10-29 01:08:02 +0300

    Lock Command + Appeal Module changes

--- cogs/moderation_module/lock.py
+++ cogs/moderation_module/lock.py
@@ -13,122 +13,137 @@ from cogs.__classes.settings import *
 class Lock_Module(commands.Cog):
     def __init__ (self, bot):
         self.bot = bot
-    
+        self.lock_role = 903399550889361428
 
     @commands.command()
-    @commands.has_permissions(administrator = True)
     async def lock(self, ctx, channel : typing.Optional[discord.TextChannel], *, message = ""):
 
-        channel = channel or ctx.channel
-        guild_id = str(ctx.channel.guild.id)
-        role = get(ctx.guild.roles, id = settings[guild_id][Settings.lock_role.value])
-        
-        permission_value = 1
-        permissions = channel.overwrites_for(role)
+        user = ctx.message.author
+        guild = ctx.message.guild
+        role = guild.get_role(self.lock_role)
 
-        if permissions.pair()[1].value & (1 << 11):
-            permission_value = -1
+        if user.guild_permissions.administrator or role in user.roles:
 
-        permissions.update(send_messages = False)
+            channel = channel or ctx.channel
+            guild_id = str(ctx.channel.guild.id)
+            role = get(ctx.guild.roles, id = settings[guild_id][Settings.lock_role.value])
+            
+            permission_value = 1
+            permissions = channel.overwrites_for(role)
 
-        if permission_value != -1:
-            await channel.set_permissions(role, overwrite = permissions)
+            if permissions.pair()[1].value & (1 << 11):
+                permission_value = -1
 
-            embedVar = discord.Embed(title = "🔒 Channel Locked", description = message, color = 0xFF0000)
-            await channel.send(embed = embedVar)
+            permissions.update(send_messages = False)
 
-            if channel != ctx.channel:
-                await ctx.send("<#{}> is now locked!".format(channel.id))
-        else:
-            if channel != ctx.channel:
-                await ctx.send("<#{}> is already locked!".format(channel.id))
+            if permission_value != -1:
+                await channel.set_permissions(role, overwrite = permissions)
+
+                embedVar = discord.Embed(title = "🔒 Channel Locked", description = message, color = 0xFF0000)
+                await channel.send(embed = embedVar)
+
+                if channel != ctx.channel:
+                    await ctx.send("<#{}> is now locked!".format(channel.id))
             else:
-                await ctx.send("This channel is already locked!")
+                if channel != ctx.channel:
+                    await ctx.send("<#{}> is already locked!".format(channel.id))
+                else:
+                    await ctx.send("This channel is already locked!")
 
 
     @commands.command()
-    @commands.has_permissions(administrator = True)
     async def unlock(self, ctx, channel : typing.Optional[discord.TextChannel], *, message = ""):
 
-        channel = channel or ctx.channel
-        guild_id = str(ctx.channel.guild.id)
+        user = ctx.message.author
+        guild = ctx.message.guild
+        role = guild.get_role(self.lock_role)
 
-        role = get(ctx.guild.roles, id = settings[guild_id][Settings.lock_role.value])
+        if user.guild_permissions.administrator or role in user.roles:
 
-        permission_value = 1
-        permissions = channel.overwrites_for(role)
-        
-        if permissions.pair()[1].value & (1 << 11):
-            permission_value = -1
+            channel = channel or ctx.channel
+            guild_id = str(ctx.channel.guild.id)
 
-        permissions.update(send_messages = None)
+            role = get(ctx.guild.roles, id = settings[guild_id][Settings.lock_role.value])
 
-        if permission_value == -1:
-            await channel.set_permissions(role, overwrite = permissions)
+            permission_value = 1
+            permissions = channel.overwrites_for(role)
+            
+            if permissions.pair()[1].value & (1 << 11):
+                permission_value = -1
 
-            embedVar = discord.Embed(title = "🔓 Channel Unlocked", description = message, color = 0x00FF00)
-            await channel.send(embed = embedVar)
+            permissions.update(send_messages = None)
 
-            if channel != ctx.channel:
-                await ctx.send("<#{}> is no longer locked!".format(channel.id))
-        else:
-            if channel != ctx.channel:
-                await ctx.send("<#{}> is not locked!".format(channel.id))
+            if permission_value == -1:
+                await channel.set_permissions(role, overwrite = permissions)
+
+                embedVar = discord.Embed(title = "🔓 Channel Unlocked", description = message, color = 0x00FF00)
+                await channel.send(embed = embedVar)
+
+                if channel != ctx.channel:
+                    await ctx.send("<#{}> is no longer locked!".format(channel.id))
             else:
-                await ctx.send("This channel is not locked!")
+                if channel != ctx.channel:
+                    await ctx.send("<#{}> is not locked!".format(channel.id))
+                else:
+                    await ctx.send("This channel is not locked!")
     
 
     @commands.command()
-    @commands.has_permissions(administrator = True)
     async def lockdown(self, ctx, Type, *, message = ""):
-
-
-        guild_id = str(ctx.channel.guild.id)
-        role = get(ctx.guild.roles, id = settings[guild_id][Settings.lock_role.value])
-
-        channels = settings[guild_id][Settings.lockdown_channels.value]
-
-        Type = Type.lower().strip()
-        send_message = None
-        _title = "🔓 Channel Unlocked"
-        _color = 0x00FF00
-        _response = "Unlocking"
         
-        if Type == "enable":
-            send_message = False
-            _title = "🔒 Channel Locked"
-            _color = 0xFF0000
-            _response = "Locking"
-        elif Type != "disable":
-            return
+        user = ctx.message.author
+        guild = ctx.message.guild
+        role = guild.get_role(self.lock_role)
 
-        totalChannels = int(0)
+        if user.guild_permissions.administrator or role in user.roles:
 
-        inititalMessage: discord.TextChannel.Message = None
+            guild_id = str(ctx.channel.guild.id)
+            role = get(ctx.guild.roles, id = settings[guild_id][Settings.lock_role.value])
 
-        if Type == "enable":
-            inititalMessage = await ctx.send(f"Locking Channels...\n0/{len(channels)}")
-        else:
-            inititalMessage = await ctx.send(f"Unlocking Channels...\n0/{len(channels)}")
+            channels = settings[guild_id][Settings.lockdown_channels.value]
 
-        for channelId in channels:
-            channel = self.bot.get_channel(channelId)
-            permissions = channel.overwrites_for(role)
+            Type = Type.lower().strip()
+            send_message = None
+            _title = "🔓 Channel Unlocked"
+            _color = 0x00FF00
+            _response = "Unlocking"
+            
+            if Type == "enable":
+                send_message = False
+                _title = "🔒 Channel Locked"
+                _color = 0xFF0000
+                _response = "Locking"
+            elif Type != "disable":
+                return
+
+            totalChannels = int(0)
+
+            inititalMessage: discord.TextChannel.Message = None
 
             if Type == "enable":
-                permissions.update(send_messages = False)
+                inititalMessage = await ctx.send(f"Locking Channels...\n0/{len(channels)}")
             else:
-                permissions.update(send_messages = None)
+                inititalMessage = await ctx.send(f"Unlocking Channels...\n0/{len(channels)}")
 
-            await channel.set_permissions(role, overwrite = permissions)
+            for channelId in channels:
+                channel = self.bot.get_channel(channelId)
+                permissions = channel.overwrites_for(role)
 
-            embedVar = discord.Embed(title = _title, description = message, color = _color)
-            await channel.send(embed = embedVar)
-            totalChannels += 1
+                if Type == "enable":
+                    permissions.update(send_messages = False)
+                else:
+                    permissions.update(send_messages = None)
 
-            await inititalMessage.edit(content = f"{_response} Channels...\n{totalChannels}/{len(channels)}")
-        
-        await inititalMessage.edit(content = f"{totalChannels} Channels {_response[:-3]}ed!")
+                await channel.set_permissions(role, overwrite = permissions)
+
+                embedVar = discord.Embed(title = _title, description = message, color = _color)
+                await channel.send(embed = embedVar)
+                totalChannels += 1
+
+                await inititalMessage.edit(content = f"{_response} Channels...\n{totalChannels}/{len(channels)}")
+            
+            await inititalMessage.edit(content = f"{totalChannels} Channels {_response[:-3]}ed!")
+
 
     @commands.command(aliases = ['lc'])
     @commands.has_permissions(administrator = True)
