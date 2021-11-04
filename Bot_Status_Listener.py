3a879e89d8fcde81bbb35fa4dfb9a4832b4ad597
SamuelSamy <samuel.fatu@stud.ubbcluj.ro>
2021-11-04 22:47:42 +0200

    Added a bot to check if  the main bots are down

---
+++ Bot_Status_Listener.py
@@ -0,0 +1,48 @@
+import asyncio
+import discord
+import json
+
+from discord.ext import tasks, commands
+
+bot = commands.Bot(intents = discord.Intents.all(), command_prefix = "=")
+
+
+with open('data/config.json') as file:
+    config = json.load(file)
+    file.close()
+
+
+@bot.event
+async def on_ready():
+    
+    check_bots.start()
+
+    print ("Bot is ready")
+
+
+@tasks.loop(seconds = 5)
+async def check_bots():
+    
+    guild = bot.get_guild(896413113044316162)
+
+    bura = guild.get_member(900101389416546314)
+    af   = guild.get_member(867480262777765918)
+
+    if str(bura.status) == "offline":
+        await send_alert(bura)
+
+    if str(af.status) == "offline":
+        await send_alert(af)
+
+
+async def send_alert(client):
+    
+    channel = bot.get_channel(905919522349928458)
+
+    for i in range(0, 3):
+        await channel.send(f"<@225629057172111362>\n<@{client.id}> just went offline")
+        await asyncio.sleep(60)
+
+
+bot.run(config['listener_token'])    
+
