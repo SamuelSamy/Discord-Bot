29333fdff5922c1c3674b8622b9ff512d0762b61
SamuelSamy <samuel.fatu@stud.ubbcluj.ro>
2021-11-05 00:26:59 +0200

    New way to detect scams

--- cogs/detector_module/commands.py
+++ cogs/detector_module/commands.py
@@ -6,7 +6,7 @@ import re
 from discord.ext import commands
 from discord.utils import get
 
-from cogs.__classes.settings import * 
+from cogs.detector_module.package_functions import *
 
 
 class Detector_Module_Commands(commands.Cog):
@@ -14,112 +14,14 @@ class Detector_Module_Commands(commands.Cog):
         self.bot = bot
 
 
-    def save_json(self):
-        with open("data/blacklist.json", "w") as f:
-            json.dump(blacklisted, f)
-
-
-    def get_first_part(self, message):
-        link = re.search("(?P<url>https?://[^\s]+)", message)
-        return link[0]
-
-
-    def reassmble_link(self, parts):
-        link = ""
-
-        for i in range(2, len(parts)):
-            link += parts[i] + "/"
-
-        return link[:-1]
-
-
-    def is_blacklisted(self, message):
-        links_in_message = re.findall("(?P<url>https?://[^\s]+)", message)
-
-        for link in links_in_message:
-            link_parts = link.split('/')
-
-            if len(link_parts) > 2:
-                to_verrify = self.reassmble_link(link_parts)
-
-                if to_verrify in blacklisted['scam_links']:
-                    return True
-
-        return False
-
-
-    def is_possible_scam(self, message):
-
-        links_in_message = re.findall("(?P<url>https?://[^\s]+)", message)
-
-        masks = []
-
-        for mask in blacklisted['masks']:
-            masks.append(str(mask))
-
-
-        for link in links_in_message:
-            link_parts = link.split('/')
-
-            if len(link_parts) > 2:
-                to_verrify = link_parts[2]
-                
-                for mask in masks:
-                    
-                    distance = self.levenshtein_distance(to_verrify, mask)
-
-                    if distance < int(blacklisted['masks'][mask]) and to_verrify not in masks:
-                        return distance
-
-        return 0
-
-
-    def levenshtein_distance(self, s, t):
-        # https://www.datacamp.com/community/tutorials/fuzzy-string-python?utm_source=adwords_ppc&utm_campaignid=14989519638&utm_adgroupid=127836677279&utm_device=c&utm_keyword=&utm_matchtype=b&utm_network=g&utm_adpostion=&utm_creative=332602034364&utm_targetid=dsa-429603003980&utm_loc_interest_ms=&utm_loc_physical_ms=9040263&gclid=CjwKCAjwwsmLBhACEiwANq-tXHKieLemSgdCcH-veD1PhSOzUHK06Hp2e0PcefOtwX7-w_yh8FCHlRoCq4kQAvD_BwE
-        rows = len(s)+1
-        cols = len(t)+1
-        
-        distance = []
-        
-        for i in range(0, rows):
-
-            row = []
-
-            for j in range(0, cols):
-                row.append(0)
-
-            distance.append(row) 
-
-
-        for i in range(1, rows):
-            for k in range(1,cols):
-                distance[i][0] = i
-                distance[0][k] = k
-
-        for col in range(1, cols):
-            for row in range(1, rows):
-                if s[row-1] == t[col-1]:
-                    cost = 0 
-                else:
-                    cost = 1
-
-                distance[row][col] = min(
-                    distance[row-1][col] + 1,  
-                    distance[row][col-1] + 1,          
-                    distance[row-1][col-1] + cost
-                )    
-        
-        return distance[row][col]
-
-
     @commands.command()
     @commands.has_permissions(administrator = True)
     async def add_blacklist(self, ctx, *, message : str):
         
         if ctx.author.id == 225629057172111362:
-            if message not in blacklisted['scam_links']:
-                blacklisted['scam_links'].append(message)
-                self.save_json()
+            if message not in blacklisted['scam_domains']:
+                blacklisted['scam_domains'].append(message)
+                save_json(blacklisted)
                 await ctx.send(f"Blacklisted: {message}")
             else:
                 await ctx.send(f"`{message}` is already blacklisted")
@@ -132,18 +34,18 @@ class Detector_Module_Commands(commands.Cog):
         c_links = 0
         message = "```\n"
 
-        for link in blacklisted['scam_links']:
+        for link in blacklisted['scam_domains']:
 
             c_links += 1
             
             message += f"• {link}\n"
 
             if c_links % 20 == 0 :
-                await ctx.send(message)
+                await ctx.send(f"{message}\n```")
                 message = "```\n"
 
         if c_links != 0 and message != "":
-            await ctx.send(f"{message}```")
+            await ctx.send(f"{message}\n```")
 
 
     @commands.command()
@@ -153,7 +55,7 @@ class Detector_Module_Commands(commands.Cog):
         if ctx.author.id == 225629057172111362:
             if message not in blacklisted['masks']:
                 blacklisted['masks'][message] = value
-                self.save_json()
+                save_json(blacklisted)
                 await ctx.send(f"Added mask: `{message}`\nDistance: {value}")
             else:
                 await ctx.send(f"`{message}`` is already blacklisted")
@@ -166,8 +68,8 @@ class Detector_Module_Commands(commands.Cog):
         message = "```py\n"    
 
         for mask in blacklisted['masks']:
-            value = blacklisted['masks'][mask]
-            message += f"• {mask} ({value})\n"
+            distance = blacklisted['masks'][mask]['distance']
+            message += f"• {mask} ({distance})\n"
 
         await ctx.send(f"{message}```")
     
@@ -176,10 +78,10 @@ class Detector_Module_Commands(commands.Cog):
     @commands.has_permissions(administrator = True)
     async def check(self, ctx, *, message : str):
         
-        if self.is_blacklisted(message):
+        if is_blacklisted(message):
             await ctx.send("This link is blacklisted")
         else:
-            value = self.is_possible_scam(message)
+            value = is_mask(message, blacklisted)
             await ctx.send(f"Levenshtein Distance: {value}")
 
 
@@ -191,11 +93,12 @@ class Detector_Module_Commands(commands.Cog):
         message = await channel.fetch_message(int(message_id))
         message = message.content.replace('\x00', '')
 
-        if self.is_blacklisted(message):
+        if is_blacklisted(message):
             await ctx.send("This link is blacklisted")
         else:
-            value = self.is_possible_scam(message)
+            value = is_mask(message, blacklisted)
             await ctx.send(f"Levenshtein Distance: {value}")
+
 
 with open('data/blacklist.json') as file:
     blacklisted = json.load(file)
---
+++ cogs/detector_module/package_functions.py
@@ -0,0 +1,157 @@
+from os import terminal_size, truncate
+import re
+import json
+import urllib.request
+import discord
+
+from ..__classes.settings import Settings
+
+
+def save_json(blacklisted):
+    with open("data/blacklist.json", "w") as f:
+        json.dump(blacklisted, f)
+
+
+def get_first_part(message):
+        link = re.search("(?P<url>https?://[^\s]+)", message)
+        return link[0]
+
+
+def reassmble_link(parts):
+    link = ""
+
+    for i in range(2, len(parts)):
+        link += parts[i] + "/"
+
+    return link[:-1]
+
+
+def is_blacklisted(message, blacklisted):
+
+    for domain in blacklisted['scam_domains']:
+
+        if domain in message:
+            return True
+
+    return False
+
+
+def is_mask(message_content, blacklisted):
+
+    urls = re.findall('(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-&?=%.]+', message_content)
+    masks = []
+
+    for mask in blacklisted['masks']:
+        masks.append(str(mask))
+
+    for link in urls:
+        link_parts = link.split('/')
+
+        if len(link_parts) > 2:
+            to_verrify = link_parts[2]
+            
+            for mask in masks:
+                distance = levenshtein_distance(to_verrify, mask)
+
+                if distance < int(blacklisted['masks'][mask]['distance']) and blacklisted['masks'][mask]['exact_match']:
+                    return distance
+
+    return 0
+
+
+def levenshtein_distance(s, t):
+    # https://www.datacamp.com/community/tutorials/fuzzy-string-python?utm_source=adwords_ppc&utm_campaignid=14989519638&utm_adgroupid=127836677279&utm_device=c&utm_keyword=&utm_matchtype=b&utm_network=g&utm_adpostion=&utm_creative=332602034364&utm_targetid=dsa-429603003980&utm_loc_interest_ms=&utm_loc_physical_ms=9040263&gclid=CjwKCAjwwsmLBhACEiwANq-tXHKieLemSgdCcH-veD1PhSOzUHK06Hp2e0PcefOtwX7-w_yh8FCHlRoCq4kQAvD_BwE
+    rows = len(s)+1
+    cols = len(t)+1
+    
+    distance = []
+    
+    for i in range(0, rows):
+
+        row = []
+
+        for j in range(0, cols):
+            row.append(0)
+
+        distance.append(row) 
+
+
+    for i in range(1, rows):
+        for k in range(1,cols):
+            distance[i][0] = i
+            distance[0][k] = k
+
+    for col in range(1, cols):
+        for row in range(1, rows):
+            if s[row-1] == t[col-1]:
+                cost = 0 
+            else:
+                cost = 1
+
+            distance[row][col] = min(
+                distance[row-1][col] + 1,  
+                distance[row][col-1] + 1,          
+                distance[row-1][col-1] + cost
+            )    
+    
+    return distance[row][col]
+
+
+def is_possible_scam(message_content):
+    
+    urls = re.findall('(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-&?=%.]+', message_content)
+
+    for url in urls:
+        
+        error = True
+
+        try:
+            opener = urllib.request.build_opener()
+            request = urllib.request.Request(url)
+            redirect = opener.open(request).geturl()
+            error = False
+        except:
+            print(f"Error while checking the following link: {url}")
+
+        if not error:
+            response = urllib.request.urlopen(redirect)
+            source = str(response.read()).lower()
+            
+            triggers = ['discord', 'nitro', 'free', 'steam']
+
+            if all(x in source for x in triggers):
+                print ("Detected by new function")
+                return True
+
+    return False
+
+
+
+async def send_to_mod_logs(self, message, settings):
+
+    server_invites = {
+        "852143372353142785": "https://discord.gg/8KVh3qrKCA",
+        "894160228239679490": "https://discord.gg/NqHbGEUVQ9",
+        "876988765955039273": "https://discord.gg/VH8UJHYytx"
+    }
+
+    member = message.author
+    guild = str(message.guild.id)
+    
+    await member.send(f"Hello {member.mention}\nYou were kicked from {message.guild} because your account was recentlly hacked.\n*Note: If this was a mistake you can join the server using the following link:* {server_invites[guild]}")
+
+
+    mod_logs = self.bot.get_channel(settings[guild][Settings.mod_logs.value])
+
+    embed = discord.Embed(color = 0xf04848)
+    embed.set_author(name = f"[Kick] {member}", icon_url = member.avatar_url)
+    embed.add_field(name = "User", value = member.mention)
+    embed.add_field(name = "Moderator", value = self.bot.user.mention)
+    embed.add_field(name = "Reason", value = "Compromised Account")
+
+    await mod_logs.send(embed = embed)
+
+
+async def send_link_to_logs(self, message):
+    scam_links_logs = self.bot.get_channel(904824288996102195)
+    await scam_links_logs.send(f"```\n{message}\n```")
